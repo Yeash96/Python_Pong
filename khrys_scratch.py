87854c6baf0a88c1c59d6ca804cc52e43fb4252b
Khrystarlite <troi.chua618@gmail.com>
2020-08-02 20:48:53 -0700

    added comments

--- khrys_scratch.py
+++ khrys_scratch.py
@@ -1,77 +1,84 @@
-
-import pygetwindow as gw
-
-
+# This is Khrystarlite's signature hugging rabbit.
 print(" () ()")
 print("<('w')>")
 
-# Yo
+# This scratch file contains the main loop that initializes pygame and used the modules to run the game.
 
 
+# Python packages
+import pygetwindow as gw    # For future use to get window size
+import pygame               # Python game creation framework - the main backend.
+import sys                  
 
-import pygame
-
-from modules import Player
+from modules import Player  # Player module for the pong paddles.
 
 
+# The main loop.
 def main():
+
+    # Create static variables:
+    fps = 60                    # The standard frames per second - limited so that the game does not automatically 
+                                #   use all system resources.
+    p_Speed = 12                # The current speed the player paddles move at.
+    win_W, win_H = 960, 540     # Dimensions for the current game window.
+    clock = pygame.time.Clock() # Used with fps variable to limit the fps of the game.
+
+
+    # Initialize pygame and its assets so they can be used
     pygame.init()
-    p1 = Player(0, boardDim=(960,540))
-    p2 = Player(1, boardDim=(960,540))
 
+    # Create player objects for the paddles.
+    p1 = Player(1, boardDim=(win_W, win_H))
+    p2 = Player(2, boardDim=(win_W, win_H))
 
-
-    gameLoop = True
-    fps = 60
-    p_Speed = 12
-
-    clock = pygame.time.Clock()
-
-    
-    pygame.display.set_caption( "Player Test" ) 
-    win_W, win_H = 960, 540 
+    # create an object that represents the display window.
     window = pygame.display.set_mode( (win_W, win_H) )
+
+    # Test window title.
+    pygame.display.set_caption( "Player Test" ) 
     
+    # Create a group of sprites that will update with inputs and display movement
     player_Group = pygame.sprite.Group()
     player_Group.add(p1)
     player_Group.add(p2)
 
-    while gameLoop:
+    # The main game loop. Runs until the exit button is clicked. May add exit function within main menu.
+    while True:
+
+        # Checks if the game is exited.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT : 
+                pygame.quit()
+                sys.exit()
+
+        # pygame will listen for keyboard inputs.
+        #   May be moved to player class later
         pressed = pygame.key.get_pressed()
+        # Listens for the w and s keys to move Player 1.
         if pressed[pygame.K_w]:
             p1.move(-p_Speed)
         elif pressed[pygame.K_s]:
             p1.move(p_Speed)
+        # Listens for the UP_arrow and DOWN_arrow keys to move Player 2.
         if pressed[pygame.K_UP]:
             p2.move(-p_Speed)
         elif pressed[pygame.K_DOWN]:
             p2.move(p_Speed)
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT : 
-                gameLoop = False
-            # if event.type == pygame.KEYDOWN:
-            #     if event.key == pygame.K_w:
-            #         p1.move(-10)
-            #     if event.key == pygame.K_s:
-            #         p1.move(10)
-            #     if event.key == pygame.K_UP:
-            #         p2.move(-10)
-            #     if event.key == pygame.K_DOWN:
-            #         p2.move(10)
         
         
+        # Draws the player sprites to the display window
         player_Group.draw(window)
+        # Updates the display window whenever the sprites update
         pygame.display.update()
+        # Updates the window so that areas where sprites were previouslyu return to the display window's background color.
         window.fill(pygame.Color(0,0,0))
-
+        # Limits FPS to 60
         clock.tick(fps)
 
         
-    pygame.quit
 
 
-
-
+# Runs the main function  when the program is run
 if __name__ == '__main__':
     main()
--- modules/player.py
+++ modules/player.py
@@ -1,72 +1,83 @@
 
-import pygame
-# Player class for pong. Only 
-import pygetwindow as gw    
+import pygame               # Import the backend game engine module.
+import sys                  # Used to exit the game.
+import pygetwindow as gw    # A module used to get window dimensions. - Not implemented yet
 
+# Player class for pong Paddles. Inherits from pygame's Sprite class
 class Player(pygame.sprite.Sprite):
     
 
-    
-    def  __init__(self,p_Num: int =0, width: int = 10,height: int = 40, boardDim: tuple =None):  
+    # Constructor that runs with defaults
+    def  __init__(self,p_Num: int = 1, width: int = 10,height: int = 40, boardDim: tuple =None):  
 
-
-        if p_Num > 1 or p_Num < 0:
+        # Checks if the values passed for the player num are appropriate.
+        if p_Num > 2 or p_Num < 1:
             print("Improper character value passed. Please pass a \"1\" or \"2\".")
-            pygame.QUIT
-            
-        super().__init__()  
-        self.h_lim = height
-
-        self.image = pygame.Surface((width, height))
-        self.image.fill (pygame.Color(255, 255, 255))
-        self.rect = self.image.get_rect()
-
-        self.p_Num = p_Num
+            pygame.quit()
+            sys.exit()
 
         if not boardDim:
-            # self.x_Board, self.y_Board = gw.getActiveWindow().size
-            self.x_Board, self.y_Board = gw.getActiveWindow().size
-        else:
-            self.x_Board, self.y_Board = boardDim
+            print("Please pass board dimensions as a tuple: (x,y).")
+            pygame.quit()
+            sys.exit()            
+            
+        # Run the constructor of the parent class to initialize other factors
+        super().__init__()  
+
+        # Create the sprite for the character paddle onto the surface of the game window with the appropriate dimensions.
+        self.image = pygame.Surface((width, height))
+        # Set the default player paddle color to white.
+        self.image.fill (pygame.Color(255, 255, 255))
+        # Use the dimensions to establish the sprite's shape:
+        self.rect = self.image.get_rect()
+        # Create a limiter for how  much buffer space is between the the end points of the sprite and the game window's walls.
+        self.h_lim = height
+        # Store data of which player this is.
+        self.p_Num = p_Num
+
+        ## Used to save the board dimensions for other calculations - may be removed later.
+        # if not boardDim:
+        #     # self.x_Board, self.y_Board = gw.getActiveWindow().size
+        #     self.x_Board, self.y_Board = gw.getActiveWindow().size
+        # else:
+        self.x_Board, self.y_Board = boardDim
 
         # board/windows parameters
 
-    
-
-        if self.p_Num == 0:
+        # Establish the default position of each player
+        if self.p_Num == 1:
             self.def_x_pos = self.x_Board  // 10     
             self.def_y_pos = self.y_Board  // 2
         else:
             self.def_x_pos = self.x_Board  * 9 // 10     
             self.def_y_pos = self.y_Board  // 2
         
+        # Starts each player sprite in the default position.
         self.setPos(self.def_x_pos,self.def_y_pos)
 
 
-    
+    # Used to reset the position of the paddles to their default positions. Should be used after each score and game initialization.
     def defPos(self):
         self.rect.centerx = self.def_x_pos
         self.rect.centery = self.def_y_pos
-        pass    
 
+    # Function used to set the player sprites to any place in the display window if needed.
     def setPos(self, x, y):
         self.rect.centerx = x
         self.rect.centery = y
 
-        pass
-
-    def getPos(self):
-        return (self.rect.centerx, self.rect.centery)
-
+    # Function is called whenever input is given
+    # TODO: Modify this so that it reads the input and does the movement making within the function.
     def move(self, y_move):
         # Movemnets are only vertical
-        # self.x_pos += x_move
+
+        # Does not allow movement further than the limit established - based on the position of the center of the sprite.
         if self.rect.centery >= self.y_Board - self.h_lim and y_move > 0:
-            pass
-        
+            pass        
         elif self.rect.centery <= 0 + self.h_lim and y_move < 0:
             pass          
         else:
+            # Otherwise, adjust movement based on the established speed. of the paddles - Can also be moved within the Class declaration.
             self.rect.centery += y_move
 
 
